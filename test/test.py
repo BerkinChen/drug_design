296ccb4c5c6207d14ce5b7efc5d648854c3d9e2b
BerkinChen <berkinchen@gmail.com>
2022-04-03 17:06:16 +0000

    chore<test>

--- test/test.py
+++ test/test.py
@@ -32,8 +32,9 @@ task = tasks.PropertyPrediction(model, task=dataset.tasks,
 optimizer = torch.optim.Adam(task.parameters(), lr=1e-3)
 solver = core.Engine(task, train_set, valid_set,
                      test_set, optimizer, gpus=[0], batch_size=1024)
-solver.train(num_epoch=500)
-solver.save('./checkpoint/checkpoint_test.pnt')
+#solver.train(num_epoch=500)
+#solver.save('./checkpoint/checkpoint_test.pnt')
+solver.load('./checkpoint/checkpoint_test.pnt')
 solver.evaluate("valid")
 
 
