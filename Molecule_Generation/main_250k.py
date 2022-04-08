a0636156385c66e58090ca5fae53679bbdaf95bd
BerkinChen <berkinchen@gmail.com>
2022-04-08 04:45:01 +0000

    Add Result

--- Molecule_Generation/main_250k.py
+++ Molecule_Generation/main_250k.py
@@ -13,7 +13,7 @@ dataset = datasets.ZINC250k("../data/molecule-datasets/", kekulize=True,
 
 model = models.RGCN(input_dim=dataset.num_atom_type,
                     num_relation=dataset.num_bond_type,
-                    hidden_dims=[256, 256, 256], batch_norm=True)
+                    hidden_dims=[256, 256, 128], batch_norm=True)
 
 num_atom_type = dataset.num_atom_type
 # add one class for non-edge
@@ -27,21 +27,21 @@ node_flow = models.GraphAF(model, node_prior, num_layer=12)
 edge_flow = models.GraphAF(model, edge_prior, use_edge=True, num_layer=12)
 
 task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
-                                      max_node=38, max_edge_unroll=12,
+                                      max_node=48, max_edge_unroll=12,
                                       criterion="nll")
 
 optimizer = optim.Adam(task.parameters(), lr=1e-3)
 solver = core.Engine(task, dataset, None, None, optimizer,
-                     gpus=(0,), batch_size=128, log_interval=10)
+                     gpus=(0,), batch_size=32, log_interval=10)
 
-if os.path.exists("../checkpoint/zinc250k_graphaf_molecule_generation.pnt"):
-    solver.load("../checkpoint/zinc250k_graphaf_molecule_generation.pnt")
+if os.path.exists("../checkpoint/zinc250k_graphaf_molecule_generation_new.pnt"):
+    solver.load("../checkpoint/zinc250k_graphaf_molecule_generation_new.pnt")
 else:
-    solver.train(num_epoch=20)
-    solver.save("../checkpoint/zinc250k_graphaf_molecule_generation.pnt")
+    solver.train(num_epoch=10)
+    solver.save("../checkpoint/zinc250k_graphaf_molecule_generation_new.pnt")
 
-results = task.generate(num_sample=30000)
-with open('result.txt','w') as f:
+results = task.generate(num_sample=300000)
+with open('result_new.txt','w') as f:
     for m in results.to_smiles():
         f.write(m)
         f.write('\n')
